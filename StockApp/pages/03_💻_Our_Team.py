a1907081b1cf28a3826f3be133c43bcce695de9c
Pratik Kankarej <pratikkankarej18@gmail.com>
2024-09-06 20:05:59 -0400

    doc update

--- StockApp/pages/03_💻_Our_Team.py
+++ StockApp/pages/03_💻_Our_Team.py
@@ -9,16 +9,16 @@ col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
 
 # Team Member 1 - Pratik
 with col1:
-    st.image("E:\\ML_Project\\clone\\stock_analysis\\assets\\pratik.jpg", caption="Pratik", width=150)
+    st.image("E:\\ML_Project\\stock\\stock_analysis\\assets\\pratik.jpg", caption="Pratik", width=150)
 
 # Team Member 2 - Sumedh
 with col2:
-    st.image("E:\\ML_Project\\clone\\stock_analysis\\assets\\sumedh.jpg", caption="Sumedh", width=150)
+    st.image("E:\\ML_Project\\stock\\stock_analysis\\assets\\sumedh.jpg", caption="Sumedh", width=150)
 
 # Team Member 3 - Madhura
 with col3:
-    st.image("E:\\ML_Project\\clone\\stock_analysis\\assets\\madhura.jpg", caption="Madhura", width=150)
+    st.image("E:\\ML_Project\\stock\\stock_analysis\\assets\\madhura.jpg", caption="Madhura", width=150)
 
 # Team Member 4 - Vedant
 with col4:
-    st.image("E:\\ML_Project\\clone\\stock_analysis\\assets\\vedant.jpg", caption="Vedant", width=150)
+    st.image("E:\\ML_Project\\stock\\stock_analysis\\assets\\vedant.jpg", caption="Vedant", width=150)
